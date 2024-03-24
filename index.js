94aa55b373dadcb445b406aee9fe44f726f23ee3
K1ra-Python <makismka.guchenko@mail.ru>
2024-03-24 21:06:23 +0300

    Основная верстка готова

--- index.js
+++ index.js
@@ -4,7 +4,29 @@ var app = new Vue({
     data: {
         isVisible: false,
         isVisibleDoctor: false,
+        activeForma: 'you',
+        activeButtonStyles:{
+            you:false,
+            adress: false,
+            passport: false,
+        },
+        changeActiveButton: {
+            'border-bottom':'none',
+            'width': '231px',
+            'height': '75px',
+        },
         selectedOptions: []
+    },
+    methods: {
+        switchForma(formName) {
+            this.activeForma = formName
+        },
+        selectStyle(buttonKey){
+            for(let key in this.activeButtonStyles){
+                this.activeButtonStyles[key] = false
+            }
+            this.activeButtonStyles[buttonKey] = true
+        }
     },
     computed: {
         selectedLabels() {
